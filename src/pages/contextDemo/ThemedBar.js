9ca35537994039dc3b9f877b103cfb09ba163c47
hhz <860601963@qq.com>
2020-02-16 13:13:34 +0800

    解决 UseThemeContext 重复渲染的问题, 同时使用 useContext 这个 HOOK

--- src/pages/contextDemo/ThemedBar.js
+++ src/pages/contextDemo/ThemedBar.js
@@ -1,27 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ThemeContext from './theme-context'
 import Button from '@material-ui/core/Button'
 
 const ThemedBar = () => {
+  console.log('render ThemedBar')
+  const {theme, toggleTheme} = useContext(ThemeContext)
   return (
-    <ThemeContext.Consumer>
-      {
-        ({theme, toggleTheme}) => {
-          return (
-            <React.Fragment>
-              <div style={{
-                marginTop: '20px',
-                background: theme.bgColor,
-                color: theme.color,
-              }}>
-                { theme.name }主题色块
-              </div>
-              <Button variant="contained" onClick={toggleTheme} color={theme.btnType}>切换主题</Button>
-            </React.Fragment>
-          )
-        }
-      }
-    </ThemeContext.Consumer>
+    <React.Fragment>
+      <div style={{
+        marginTop: '20px',
+        background: theme.bgColor,
+        color: theme.color,
+      }}>
+        { theme.name }主题色块
+      </div>
+      <Button variant="contained" onClick={toggleTheme} color={theme.btnType}>切换主题</Button>
+    </React.Fragment>
   )
 }
 
--- src/pages/contextDemo/UseThemeContext.js
+++ src/pages/contextDemo/UseThemeContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import logo from '../../logo.svg'
 import '../../App.css'
 import Button from '@material-ui/core/Button'
@@ -8,31 +8,27 @@ import ThemeContext from './theme-context'
 import ThemedBar from './ThemedBar'
 
 const MainBar = () => {
+  console.log('render mainBar')
+  const {theme, changeTheme} = useContext(ThemeContext)
+
   return (
-    <ThemeContext.Consumer>
-      {
-        ({theme, changeTheme}) => {
-          return (
-            <React.Fragment>
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Welcome to React {theme.name}</h1>
-                <div>
-                  <Button variant="contained" onClick={() => {changeTheme(themes.light)}}>浅色主题</Button>
-                  <Button variant="contained" color="primary" onClick={() => {changeTheme(themes.dark)}}>
-                    深色主题
-                  </Button>
-                </div>
-              </header>
-            </React.Fragment>
-          )
-        }
-      }
-    </ThemeContext.Consumer>
+    <React.Fragment>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to React {theme.name}</h1>
+        <div>
+          <Button variant="contained" onClick={() => {changeTheme(themes.light)}}>浅色主题</Button>
+          <Button variant="contained" color="primary" onClick={() => {changeTheme(themes.dark)}}>
+            深色主题
+          </Button>
+        </div>
+      </header>
+    </React.Fragment>
   )
 }
 
-export default function UseThemeContext() {
+// 使用 ThemeProvider 来管理状态，避免 UseThemeContext 的重复渲染
+function ThemeProvider(props) {
   const [theme, setTheme] = useState(themes.light)
   const toggleTheme = () => {
     setTheme(theme === themes.light ? themes.dark: themes.light)
@@ -40,13 +36,22 @@ export default function UseThemeContext() {
   const changeTheme = theme => {
     setTheme(theme)
   }
-
+  console.log("render ThemeProvider");
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme, changeTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default function UseThemeContext() {
+  console.log("render UseThemeContext");
+  return (
+    <ThemeProvider>
       <div className="App">
         <MainBar />
         <ThemedBar />
       </div>
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
